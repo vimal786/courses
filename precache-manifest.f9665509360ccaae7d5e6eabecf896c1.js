c03d957ddfc81bb6c38a9b2c32340935dcfa5ebe
vimal786 <vimalimr@gmail.com>
2020-10-10 16:35:05 +0530

    Updates

---
+++ precache-manifest.f9665509360ccaae7d5e6eabecf896c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e61e73ba5d9ac406364371881402e62",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "7ce4128a6effe9bc5457",
+    "url": "/courses/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "af853a515cf10489e595",
+    "url": "/courses/static/css/main.6cc01e4c.chunk.css"
+  },
+  {
+    "revision": "7ce4128a6effe9bc5457",
+    "url": "/courses/static/js/2.0e5e656e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/courses/static/js/2.0e5e656e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af853a515cf10489e595",
+    "url": "/courses/static/js/main.9adec1aa.chunk.js"
+  },
+  {
+    "revision": "d4d26fb434ba54fa9270",
+    "url": "/courses/static/js/runtime-main.8d00cf69.js"
+  }
+]);
